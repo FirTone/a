033d5d6a5b960a6d1905863d2d98d9ec8c69ec6e
FirTone <75525050+FirTone@users.noreply.github.com>
2020-12-20 05:23:54 +0800

    Update and rename ng.sh to all.sh

--- all.sh
+++ all.sh
@@ -4,3 +4,7 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 ./ngrok authtoken 1lsyo60cLJbLpgf3uVmyuuvYb2B_5LgZKHLEGHbp58dCuhfHn
 apt-get install screen
+ngrok!!!
+
+wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh
+宝塔
