84a6c11663d7b2165b460f12c10626101623ce6d
FirTone <75525050+FirTone@users.noreply.github.com>
2020-12-20 08:32:22 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -9,5 +9,29 @@ ngrok!!!
 wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh
 宝塔
 
+sudo -i
+apt-get install screen
+screen -S dk
+cd /
+mkdir frpc
+cd frpc
+wget https://getfrp.sh/d/frpc_linux_amd64
+#安装frp时
+
+frpc -f c0d9rmwlivb1j3hs:636064
+#mc服务器
+
+sudo -i
+apt-get install screen
+screen -S mcc
+cd /
+mkdir mc
+cd mc
 wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.16.201.02.zip
 unzip bedrock-server-1.16.201.02.zip
+apt install -y libcurl4-openssl-dev
+apt-get install default-jdk
+java -version
+#完成执行后
+LD_LIBRARY_PATH=. ./bedrock_server
+
